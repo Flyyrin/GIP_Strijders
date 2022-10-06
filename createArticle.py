cfd047009cb755786251913ac8a88f5bad5d2631
Rafael Lemmen <BouncePlayR@gmail.com>
2022-10-06 09:04:45 +0200

    update

--- createArticle.py
+++ createArticle.py
@@ -1,6 +1,7 @@
 import os
 import datetime 
 import shutil
+from PIL import Image
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -20,11 +21,6 @@ def createArticle(cat, title, author, text, filePath):
         title = title.replace(" ","-")
         path = f"{dates}-{times}.html".replace(" ","")
         
-        spotHtml = '<div class="grid-item"><article class="article"><div class="card">==IMG==<div class="card-body text-center px-1"><a href="==HREF==" class="text-title display-1 text-dark">==TITLE==</a><p class="secondary-title text-secondary display-3"><span><i class="far fa-clock text-primary"></i>    ==DATE==</span></p></div></div></article></div>'
-        spotHtml = spotHtml.replace("==TITLE==", title)
-        spotHtml = spotHtml.replace("==HREF==", f"articles/{path}")
-        spotHtml = spotHtml.replace("==DATE==", dates)
-
         try:
             ext = filePath.split(".")[1]
             newFilePath = os.path.join(PATH, "images", rf"{dates}-{times}.{ext}".replace(" ",""))
@@ -32,13 +28,17 @@ def createArticle(cat, title, author, text, filePath):
             shutil.copy(filePath, newFilePath)
             article = article.replace("==IMG==", f'<div class="thumbnail mt-3"><img src="{newFilePathV}" class="thumbnail" alt=""></div>')
             spotHtml = spotHtml.replace("==IMG==", f'<div class="overflow-img"><a href="articles/{path}"><img src="{newFilePathV}" class="img-fluid" alt=""></a></div>')
-            
         except:
             spotHtml = spotHtml.replace("==IMG==", "")
             article = article.replace("==IMG==", "")
 
-        print(spotHtml)
-        # spotHtml toevoegen aan blogs op de index
+        spotHtml = '<div class="grid-item"><article class="article"><div class="card">==IMG==<div class="card-body text-center px-1"><a href="==HREF==" class="text-title display-1 text-dark">==TITLE==</a><p class="secondary-title text-secondary display-3"><span><i class="far fa-clock text-primary"></i>    ==DATE==</span></p></div></div></article></div>'
+        spotHtml = spotHtml.replace("==TITLE==", title)
+        spotHtml = spotHtml.replace("==HREF==", f"articles/{path}")
+        spotHtml = spotHtml.replace("==DATE==", dates)
+        ImageHeight = Image.open(newFilePathV).height
+        spotHtml = spotHtml.replace("==PAD==", 100/(280/ImageHeight))
+
         with open(os.path.join(PATH, "articles.txt"), "a") as spots:
             spots.write(spotHtml)
 
